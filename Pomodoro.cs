37f64809ce36e81bd5edb72d294293b0dcb4e33e
Andrey <AndryxaSte@gmail.com>
2020-07-05 18:27:18 +0300

    Initial commit

---
+++ Pomodoro.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+namespace PomodoroTimer
+{
+    class Pomodoro
+    {
+        double quantity;
+        double loop;
+        double duration;
+
+        public void Start()
+        {
+            if (loop < quantity)
+            {
+                Work();
+            }
+        }
+        async private void Work()
+        {
+            Console.WriteLine(DateTime.Now + " Start Work");
+            Console.Title = "Work";
+
+            await Task.Delay(delay: TimeSpan.FromMinutes(duration));
+            Console.Beep(500, 1000);
+            Rest();
+        }
+
+        async private void Rest()
+        {
+            Console.WriteLine(DateTime.Now + " Start Rest");
+            Console.Title = "Rest";
+            Console.WriteLine();
+
+            loop++;
+            await Task.Delay(TimeSpan.FromMinutes((loop % 4 == 0) ? duration : duration/5));
+            Console.Beep(500, 350);
+            Console.Beep(500, 350);
+            Start();
+        }
+
+        public Pomodoro(double quantity, double duration)
+        {
+            this.duration = duration;
+            this.quantity = quantity;
+            Console.WriteLine($"{quantity} циклов по {duration + duration / 5} минут каждый");
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+
+namespace PomodoroTimer
+{
+    class Program
+    {
+
+
+        public static void Main()
+        {
+            Console.WriteLine("Введите количество циклов");
+            Console.WriteLine("Длительность цикла работы");
+#pragma warning disable CA1305 // Укажите IFormatProvider
+            new Pomodoro(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())).Start() ;
+#pragma warning restore CA1305 // Укажите IFormatProvider
+
+            Console.ReadLine();
+        }
+
+
+
+    }
+}
